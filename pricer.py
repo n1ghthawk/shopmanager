d329bfa3596355c6954e9a0d53eae79d2a9589ac
n1ghthawk <ansar.bedhar@gmail.com>
2023-07-03 02:54:27 +0530

    Update pricer.py

--- pricer.py
+++ pricer.py
@@ -1,12 +1,12 @@
 import pandas as pd
 import numpy as np
 
-processed_stock = "../storage/Processed Stock Summary.csv"
-final_price_list = "../storage/final_price_list.csv"
+processed_stock = "./storage/Processed Stock Summary.csv"
+final_price_list = "./storage/final_price_list.csv"
 
-processed_stock_json = "../public/Processed Stock Summary.json"
-final_price_list_json = "../public/final_price_list.json"
-stock_with_price_list_json = "../public/stock_with_price_list.json"
+processed_stock_json = "./public/Processed Stock Summary.json"
+final_price_list_json = "./public/final_price_list.json"
+stock_with_price_list_json = "./public/stock_with_price_list.json"
 
 def csv2df(loc, headers=None):
     # Read in the file
