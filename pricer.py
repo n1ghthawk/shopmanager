1c85e77d66c9dba3aacb0fd5511013a85b0ddf4c
n1ghthawk <ansar.bedhar@gmail.com>
2023-07-12 00:31:02 +0530

    Add files via upload

--- pricer.py
+++ pricer.py
@@ -1,30 +1,37 @@
 import pandas as pd
 import numpy as np
 
-processed_stock = "storage_processed_stock_summary.csv"
-final_price_list = "storage_final_price_list.csv"
-mask_list = "storage_mask_data.csv"
+processed_stock = "./storage/Processed Stock Summary.csv"
+final_price_list = "./storage/final_price_list.csv"
 
 processed_stock_json = "./public/Processed Stock Summary.json"
 final_price_list_json = "./public/final_price_list.json"
 stock_with_price_list_json = "./public/stock_with_price_list.json"
-mask_json = "./public/mask.json"
 
-def csv2df(loc, headers=None):
+headers_processed_stock = ['items','code','under','quantity','extra']
+headers_raw_stock = ["items", "code", "under", "quantity",'extra']
+headers_mask_stock = ["items", "code", "under", "quantity",'extra']
+
+# def csv2df(loc, headers=None):
+#     if headers:
+#         return pd.read_csv(loc, names=headers,encoding="utf-16le")
+#     else:
+#         return pd.read_csv(loc,encoding="utf-16le")
+
+def csv2df(loc, headers=None, encoding='utf-16le'):
     # Read in the file
-    with open(loc, 'r') as file :
-        filedata = file.read()
+    # with open(loc, 'r') as file :
+    #     filedata = file.read()
     # Replace the target string
-    filedata = filedata.replace('\00', '')
-    filedata = filedata.replace(",\n", '')
+    # filedata = filedata.replace('\00', '')
+    # filedata = filedata.replace(",\n", '')
     # Write the file out again
-    with open(loc, 'w') as file:
-        file.write(filedata)
+    # with open(loc, 'w', encoding='utf-16le') as file:
+    #     file.write(filedata)
     if headers:
-        return pd.read_csv(loc, names=headers)
+        return pd.read_csv(loc, encoding=encoding, names=headers)
     else:
-        return pd.read_csv(loc)
-
+        return pd.read_csv(loc, encoding=encoding)
 
 def assignPrice(pricelist_df, processed_stock_df):
     priced_tally_df = pd.merge(processed_stock_df, pricelist_df, on='items', how='left')
@@ -39,30 +46,23 @@ def clean_df(df, columnname):
 
 
 def get_processed_df():
-    processed_stock_df = csv2df(processed_stock,["code", "items", "under", "quantity"])
+    processed_stock_df = csv2df(processed_stock,headers_processed_stock)
     processed_stock_df = processed_stock_df.iloc[1:]
     processed_stock_df = processed_stock_df[processed_stock_df.quantity != 0]
     clean_df(processed_stock_df, 'items')
     return processed_stock_df
 
+
 def get_final_price_list_df():
-    final_price_list_df = csv2df(final_price_list)
+    final_price_list_df = csv2df(final_price_list, encoding='utf-8')
     final_price_list_df.drop(columns=['under'], inplace=True)
     clean_df(final_price_list_df, 'items')
     return final_price_list_df
-
-def get_mask_df():
-    mask_df = csv2df(mask_list, ["code", "items", "under", "quantity"])
-    mask_df.drop(columns=['under'], inplace=True)
-    mask_df = mask_df[mask_df.quantity != 0]
-    return mask_df
 
 def get_stock_with_price_list_df(final_price_list_df, processed_stock_df):
     merged = pd.merge(final_price_list_df, processed_stock_df, on='items', how='right',suffixes=('_pricelist', ''))
     merged.drop(columns=['code_pricelist'], inplace=True)
     return merged
-
-
 
 
 
@@ -74,10 +74,8 @@ def saveAsJSON(df, loc):
 processed_stock_df = get_processed_df()
 final_price_list_df = get_final_price_list_df()
 stock_with_price_df = get_stock_with_price_list_df(final_price_list_df, processed_stock_df)
-mask_df = get_mask_df()
 # generating JSON for stock with price list
 
 saveAsJSON(processed_stock_df, processed_stock_json)
 saveAsJSON(final_price_list_df, final_price_list_json)
 saveAsJSON(stock_with_price_df, stock_with_price_list_json)
-saveAsJSON(mask_df, mask_json)
