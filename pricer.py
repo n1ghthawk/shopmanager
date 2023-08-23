e4350eb839cb063e70b9ba77979de0c4872a6046
n1ghthawk <ansar.bedhar@gmail.com>
2023-08-23 22:17:05 +0530

    updated pricer

--- pricer.py
+++ pricer.py
@@ -15,6 +15,7 @@ tyre_price_list_json = "./public/tyre_price_list.json"
 reorder_json = "./public/reorderpurc.json"
 status_json = "./public/status.json"
 
+headers_status = ['item','updatedon']
 headers_processed_stock = ['items','code','under','quantity','extra']
 headers_raw_stock = ["items", "code", "under", "quantity",'extra']
 headers_mask_stock = ["items", "code", "under", "quantity",'extra']
@@ -25,6 +26,45 @@ headers_reorder_stock = ["code", "items", "reorderlevel", "quantity",'netquantit
 #         return pd.read_csv(loc, names=headers,encoding="utf-16le")
 #     else:
 #         return pd.read_csv(loc,encoding="utf-16le")
+
+old_status_file = status_json
+headers_old_status = ["param","value"]
+old_status_df = pd.DataFrame(columns=headers_old_status)
+try:
+    old_status_df = pd.read_json(old_status_file)
+    print(old_status_df)
+except:
+    pass
+
+
+current_status_file = status_csv
+headers_current_status = ["param","value"]
+current_status_df = pd.DataFrame(columns=headers_current_status)
+try:
+    current_status_df = pd.read_csv(current_status_file, names=headers_current_status)
+    print(current_status_df)
+except:
+    pass
+
+
+def get_value(df,colname,itemname):
+    time = df.loc[df[colname] == itemname]
+    if(not time.empty):
+        return time['value'][0]
+    else:
+        return ""
+
+
+
+
+
+
+
+
+
+
+
+
 
 def csv2df(loc, headers=None, encoding='utf-16le', dtype = None):
     # Read in the file
@@ -88,34 +128,69 @@ def getEmailTime():
     return status_df
 
 
-processed_stock_df = get_processed_df()
-processed_stock_df.drop(['extra'], axis=1, inplace=True)
-final_price_list_df = get_final_price_list_df()
-final_price_list_df.drop(['extra'], axis=1, inplace=True)
-stock_with_price_df = get_stock_with_price_list_df(final_price_list_df, processed_stock_df)
-tyre_pricelist_df = csv2df(tyre_pricelist, encoding='utf-8')
+
+def read_statusjson():
+    pass
 
 
-# rename dataframes
-final_price_list_df.rename(columns = {'retail_price':'ret_prc','suggested_price':'sug_prc', 'wholesale_price':'wsale_prc'}, inplace = True)
-stock_with_price_df.rename(columns = {'quantity':'qty','retail_price':'ret_prc','suggested_price':'sug_prc', 'wholesale_price':'wsale_prc'}, inplace = True)
+status_data=[]
+# processing  processed data and stock with price data and final pricelist
+try:    
+    processed_stock_df = get_processed_df()
+    final_price_list_df = get_final_price_list_df()
+    if(len(processed_stock_df.index)>0 and len(get_final_price_list_df.index)>0):
+        processed_stock_df.drop(['extra'], axis=1, inplace=True)    
+        final_price_list_df.drop(['extra'], axis=1, inplace=True)
+        stock_with_price_df = get_stock_with_price_list_df(final_price_list_df, processed_stock_df)
+        # rename dataframes
+        final_price_list_df.rename(columns = {'retail_price':'ret_prc','suggested_price':'sug_prc', 'wholesale_price':'wsale_prc'}, inplace = True)
+        stock_with_price_df.rename(columns = {'quantity':'qty','retail_price':'ret_prc','suggested_price':'sug_prc', 'wholesale_price':'wsale_prc'}, inplace = True)
+        # replace NaN for prices with 0
+        final_price_list_df[["wsale_prc","sug_prc","ret_prc"]] = final_price_list_df[["wsale_prc","sug_prc","ret_prc"]].fillna(0)
+        stock_with_price_df[["wsale_prc","sug_prc","ret_prc"]] = stock_with_price_df[["wsale_prc","sug_prc","ret_prc"]].fillna(0)
+        stock_with_price_df[["priced"]] = stock_with_price_df[["priced"]].fillna("No Match")
+        saveAsJSON(processed_stock_df, processed_stock_json)
+        saveAsJSON(final_price_list_df, final_price_list_json)
+        saveAsJSON(stock_with_price_df, stock_with_price_list_json)
+        status_data.append({'param':'processed_data', 'value':get_value(current_status_df,'param','processed_data')})
+    else:
+        status_data.append({'param':'processed_data', 'value':get_value(old_status_df,'param','processed_data')})
+except:
+    status_data.append({'param':'processed_data', 'value':get_value(old_status_df,'param','processed_data')})
 
-# replace NaN for prices with 0
-final_price_list_df[["wsale_prc","sug_prc","ret_prc"]] = final_price_list_df[["wsale_prc","sug_prc","ret_prc"]].fillna(0)
-stock_with_price_df[["wsale_prc","sug_prc","ret_prc"]] = stock_with_price_df[["wsale_prc","sug_prc","ret_prc"]].fillna(0)
-stock_with_price_df[["priced"]] = stock_with_price_df[["priced"]].fillna("No Match")
 
-# Reorder and Purchase Data
-reorder_df = csv2df(reorder_csv, headers=headers_reorder_stock, dtype={'code': str})
-reorder_df.drop(['extra'], axis=1, inplace=True)
+# processing tyre_pricelist
+try:
+    tyre_pricelist_df = csv2df(tyre_pricelist, encoding='utf-8')
+    if(len(tyre_pricelist_df.index)>0):
+        saveAsJSON(tyre_pricelist_df, tyre_price_list_json)
+        status_data.append({'param':'pricelist_mrf', 'value':get_value(current_status_df,'param','pricelist_mrf')})
+    else:
+        status_data.append({'param':'pricelist_mrf', 'value':get_value(old_status_df,'param','pricelist_mrf')})
+except:
+    status_data.append({'param':'pricelist_mrf', 'value':get_value(old_status_df,'param','pricelist_mrf')})
+
+
+# Processing Reorder and Purchase Data
+try:
+    reorder_df = csv2df(reorder_csv, headers=headers_reorder_stock, dtype={'code': str})
+    if(len(reorder_df.index)>0):
+        reorder_df.drop(['extra'], axis=1, inplace=True)
+        saveAsJSON(reorder_df, reorder_json)
+        status_data.append({'param':'reorder_data', 'value':get_value(current_status_df,'param','reorder_data')})
+    else:
+        status_data.append({'param':'reorder_data', 'value':get_value(old_status_df,'param','reorder_data')})
+except:
+    status_data.append({'param':'reorder_data', 'value':get_value(old_status_df,'param','reorder_data')})
+
+
 
 # generating status
-status_df = getEmailTime()
-
-# generating JSON for stock with price list
-saveAsJSON(processed_stock_df, processed_stock_json)
-saveAsJSON(final_price_list_df, final_price_list_json)
-saveAsJSON(stock_with_price_df, stock_with_price_list_json)
-saveAsJSON(tyre_pricelist_df, tyre_price_list_json)
-saveAsJSON(reorder_df, reorder_json)
+# status_df = getEmailTime()
+status_df = pd.DataFrame.from_records(status_data)
 saveAsJSON(status_df, status_json)
+
+
+
+
+
